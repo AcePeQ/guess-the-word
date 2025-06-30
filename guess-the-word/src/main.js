56148fd474caa35a6028f953fc492a6c9d2398d8
AcePeQ <maciej.nojszewski22@gmail.com>
2025-06-30 13:57:05 +0200

    feat: add features of generating random word and scramble this word

--- guess-the-word/src/main.js
+++ guess-the-word/src/main.js
@@ -9,3 +9,33 @@ const words = [
   "player",
   "react",
 ];
+
+let currentWord = "";
+let numberOfTries = 0;
+let mistakes = [];
+
+function scrambleWord(word) {
+  const letters = word.split("");
+
+  for (let i = 0; i < letters.length - 1; i++) {
+    const randomLetterIndex = Math.floor(Math.random() * letters.length);
+    let tempLetter = letters[i];
+    letters[i] = letters[randomLetterIndex];
+    letters[randomLetterIndex] = tempLetter;
+  }
+
+  return letters.join("");
+}
+
+function generateRandomWord() {
+  const displayWordContainer = document.querySelector("#display");
+  currentWord = words[Math.floor(Math.random() * words.length)];
+
+  const scrambledWord = scrambleWord(currentWord);
+
+  displayWordContainer.textContent = scrambledWord;
+}
+
+function createInputFields(length) {}
+
+generateRandomWord();
