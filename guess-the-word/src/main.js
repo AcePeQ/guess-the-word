89f54e40ecb0e6f9d9e0fe25d2d0271aa8ff616e
AcePeQ <maciej.nojszewski22@gmail.com>
2025-07-01 13:46:49 +0200

    feat: add createInputFields function and half functionality of handleInput

--- guess-the-word/src/main.js
+++ guess-the-word/src/main.js
@@ -14,6 +14,8 @@ let currentWord = "";
 let numberOfTries = 0;
 let mistakes = [];
 
+const inputsContainer = document.querySelector("#inputs");
+
 function scrambleWord(word) {
   const letters = word.split("");
 
@@ -36,6 +38,40 @@ function generateRandomWord() {
   displayWordContainer.textContent = scrambledWord;
 }
 
-function createInputFields(length) {}
+function createInputFields(length) {
+  for (let i = 0; i < length; i++) {
+    const input = document.createElement("input");
+    input.setAttribute("id", `input-${i}`);
+    input.setAttribute("type", `text`);
+    input.setAttribute("class", `input input-${i}`);
+    input.setAttribute("maxLength", `1`);
+
+    inputsContainer.appendChild(input);
+    input.dataset.index = i;
+    input.addEventListener("input", handleInput);
+  }
+}
+
+function handleInput(event) {
+  const input = event.target;
+  if (!input.value) return;
+
+  const inputIndex = input.dataset.index;
+
+  if (
+    currentWord.includes(input.value) &&
+    currentWord[inputIndex] === input.value
+  ) {
+    if (input.nextElementSibling) {
+      input.nextElementSibling.focus();
+    } else {
+      input.blur();
+    }
+  } else {
+    input.value = "";
+    mistakes.push(input.value);
+  }
+}
 
 generateRandomWord();
+createInputFields(currentWord.length);
